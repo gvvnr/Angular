bd0dac90c6f2d2b74258820bb80eb7ce2434eb09
gvvnr <you@example.com>
2019-02-15 18:03:38 +0530

    commited

--- angularRouting/src/app/app-routing.module.ts
+++ angularRouting/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
-import {Routes , RouterModule} from '@angular/router';
-import {ChildComponent} from "./child/child.component";
-import {Child1Component} from "./child1/child1.component";
-const route: Routes = [
-  {path :'child1', component :Child1Component}
-];
-@NgModule({
-  declarations: [],
-  imports: [
-    RouterModule.forRoot(route),
-  ]
-})
-export class AppRoutingModule {
+import { CommonModule } from '@angular/common';
 
-}
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
